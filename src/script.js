5fa15f45d58850e2986b4272b991218b654571b9
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-01-26 15:56:03 -0500

    Add canvas for rendering crowd

--- src/script.js
+++ src/script.js
@@ -1,3 +1,8 @@
+var canvas = document.querySelector("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 var crowd = [];
 var settings = {
       'crowd': {
