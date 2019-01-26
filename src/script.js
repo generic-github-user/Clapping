cd9d4925bfd20c0f671e2e155b236662b23a51ed
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-01-26 16:01:45 -0500

    Use single quotes

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
-var canvas = document.querySelector("canvas");
-var ctx = canvas.getContext("2d");
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
