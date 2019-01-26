72612c3c0abe6077b08871c8150e122d8f2998e7
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-01-26 15:44:14 -0500

    Add random initial crowd generation

---
+++ src/script.js
@@ -0,0 +1,9 @@
+var crowd = [];
+
+for (var i = 0; i < 100; i++) {
+      var row = [];
+      for (var j = 0; j < 100; j++) {
+            row.push(Math.random());
+      }
+      crowd.push(row);
+}
